3604c059ea7dad395f0a435aeba9be2f3cf57c31
foviedo <oviedoee@yahoo.com>
2019-11-22 18:10:08 -0300

    Más métodos de pirata

--- Pirata.rb
+++ Pirata.rb
@@ -21,17 +21,42 @@ class Pirata
     def pasadoDeGrog()
         return @nivelEbriedad >= 90
     end
-    
+
+    def tomarGrog()
+        @nivelEbriedad += 5
+        self.gastarMoneda
+    end
+
+    def gastarMoneda()
+        self.validarGastarMonedas()
+        @cantidadMonedas-= 1
+    end
+
+
+    def validarGastarMonedas()
+        if(@cantidadMonedas == 0)
+            raise "Monedas insuficientes"
+        end
+    end
+
+    def fuisteInvitadoPor(unTripulante)
+        return unTripulante == @invitante
+    end
 end
 
 
 if __FILE__ == $0
     puts("asd")
-    pirata = Pirata.new(["a","s","d","f"],"b",100,"d")
+    pirata = Pirata.new(["a","s","d","f"],"El we",100,20)
     a = pirata.tiene("s")
     puts(a)
     puts(pirata.cantidadItems())
     puts(pirata.pasadoDeGrog())
+    pirata.tomarGrog
+    puts(pirata.cantidadMonedas)
+    puts(pirata.nivelEbriedad)
+    puts(pirata.fuisteInvitadoPor("we"))
+    puts(pirata.fuisteInvitadoPor("El we"))
     gets()
 
 end
