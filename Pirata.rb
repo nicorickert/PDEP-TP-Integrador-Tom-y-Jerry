36321ad4393bed791cbdffad6b2c2dd326b743a3
foviedo <oviedoee@yahoo.com>
2019-11-22 19:56:07 -0300

    No es necesario que alguien lo invite

--- Pirata.rb
+++ Pirata.rb
@@ -3,8 +3,7 @@ class Pirata
     attr_accessor :invitante
     attr_accessor :nivelEbriedad
     attr_accessor :cantidadMonedas
-
-    def initialize(items, invitante, nivelEbriedad, cantidadMonedas)
+    def initialize(items, invitante = "nadie", nivelEbriedad, cantidadMonedas)
         @items = items
         @invitante = invitante
         @nivelEbriedad = nivelEbriedad
@@ -52,7 +51,7 @@ end
 
 
 if __FILE__ == $0
-    pirata = Pirata.new(["a","s","d","f"],"El we",100,20)
+    pirata = Pirata.new(["a","s","d","f"],100,20)
     puts ("Pruebas pirata: ")
     a = pirata.tiene("s")
     puts(a)
@@ -62,7 +61,7 @@ if __FILE__ == $0
     puts(pirata.cantidadMonedas)
     puts(pirata.nivelEbriedad)
     puts(pirata.fuisteInvitadoPor("we"))
-    puts(pirata.fuisteInvitadoPor("El we"))
+    puts(pirata.fuisteInvitadoPor("nadie"))
     espia = EspiaDeLaCorona.new(["a","d"], "yo", 999,20)
     puts("Pruebas espia: ")
     puts(espia.pasadoDeGrog)
