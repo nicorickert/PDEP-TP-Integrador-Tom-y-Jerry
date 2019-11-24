24b95ed52194f8e8820261b7b9ec2adf850a113f
foviedo <oviedoee@yahoo.com>
2019-11-24 20:34:33 -0300

    Agregados métodos de barco

--- Pirata.rb
+++ Pirata.rb
@@ -9,7 +9,7 @@ class Pirata
         @nivelEbriedad = nivelEbriedad
         @cantidadMonedas = cantidadMonedas
     end
-    def tiene(unItem)
+    def tiene?(unItem)
         return @items.include?(unItem)
     end
 
@@ -56,7 +56,7 @@ end
 
 
 class Barco
-    def initialize(capacidad,tripulantes,mision = "ninguna")
+    def initialize(capacidad,tripulantes,mision)
         @mision = mision
         @Capacidad = capacidad
         @Tripulantes= tripulantes
@@ -70,20 +70,61 @@ class Barco
     def cantidadTripulantes()
         return @Tripulantes.length
     end
-    def todosPasadosDeGrog()
+    def todosPasadosDeGrog?()
         return @Tripulantes.all? {|tripulante| tripulante.pasadoDeGrog}
     end
-    def hayLugar
+    def puedeUnirse?(unPirata)
+        return self.hayLugar? && @mision.esUtil(unPirata)
+    end
+
+    def hayLugar?()
         return self.cantidadTripulantes < @Capacidad
     end
+
+    def agregar(unTripulante)
+        if(self.puedeUnirse?(unTripulante))
+            @Tripulantes.push(unTripulante)
+            puts("agregado")
+        end
+    end
+    def mostrarTripulantes()
+        @Tripulantes.each do |tripulante|
+            puts (tripulante)
+        end
+    end
+
+    def cambiarMision!(unaMision)
+        @Tripulantes.select!() {|tripulante| unaMision.esUtil(tripulante) }
+        @mision = unaMision
+    end
+
     def pirataMasEbrio()
         return @Tripulantes.max {|tripulante| tripulante.nivelEbriedad}
     end
+
+    def anclarEn(unaCiudad)
+        self.todosTomanGrog!()
+        self.perderMasEbrioEn!(unaCiudad)
+    end
+
+    def todosTomanGrog!()
+        @Tripulantes.each {|tripulante| tripulante.tomarGrog()}
+    end
+
+    def perderMasEbrioEn!(unaCiudad)
+        @Tripulantes.delete(self.pirataMasEbrio())
+        unaCiudad.sumarHabitante()
+    end
+
     def esTemible()
         @mision.esRealizablePor(self)
     end
     def tieneSuficienteTripulacion()
         return self.cantidadTripulantes >= @Capacidad * 0.9
+    end
+
+    def tiene?(unItem)
+        return @Tripulantes.any? {|tripulante| tripulante.tiene?(unItem)}
     end
 
 end
@@ -97,7 +138,7 @@ class CiudadCostera
         return unPirata.nivelEbriedad() >= 50
     end
     def esVulnerableA(otroBarco)
-        otroBarco.cantidadTripulantes() >= @cantidadHabitantes *0.4 || otroBarco.todosPasadosDeGrog()
+        otroBarco.cantidadTripulantes() >= @cantidadHabitantes *0.4 || otroBarco.todosPasadosDeGrog?()
     end
     def sumarHabitante
         @cantidadHabitantes += 1
@@ -116,14 +157,15 @@ class BusquedaDelTesoro < Mision
         return self.tieneAlgunItemObligatorio(unPirata) && unPirata.cantidadMonedas() <=5
     end
     def tieneAlgunItemObligatorio(unPirata)
-        return ["brujula","mapa","grogXD"].any? {|item| unPirata.tiene(item)}
+        return ["brujula","mapa","grogXD"].any? {|item| unPirata.tiene?(item)}
     end
 end
 
 if __FILE__ == $0
+    busqueda = BusquedaDelTesoro.new()
     pirata = Pirata.new(["a","s","d","mapa"],100,5)
     puts ("Pruebas pirata: ")
-    a = pirata.tiene("s")
+    a = pirata.tiene?("s")
     puts(a)
     puts(pirata.cantidadItems())
     puts(pirata.pasadoDeGrog())
@@ -136,15 +178,16 @@ if __FILE__ == $0
     puts("Pruebas espia: ")
     puts(espia.pasadoDeGrog)
     puts("Pruebas barco: ")
-    barquito = Barco.new(20,[pirata,])
-    barcote = Barco.new(20,[pirata,pirata])
+    barquito = Barco.new(20,[pirata], busqueda)
+    barcote = Barco.new(20,[pirata,pirata,espia],busqueda)
     puts(barquito.sosSaqueablePor(pirata))
     puts(barquito.cantidadTripulantes)
     puts(barquito.esVulnerableA(barcote))
-    puts(barcote.todosPasadosDeGrog)
-    puts(barquito.pirataMasEbrio)
+    puts(barcote.todosPasadosDeGrog?())
+    puts(barcote.pirataMasEbrio)
+    barquito.agregar(pirata)
+    puts(barquito.cantidadTripulantes)
     puts ("pruebas mision: ")
-    busqueda = BusquedaDelTesoro.new()
     puts(busqueda.esUtil(pirata))
     puts(busqueda.esUtil(espia))
     gets()
